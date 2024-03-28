7a7b6c8855bf387ab3c1b888d96d3358b61968ce
neil mclaughlin <neil.mclaughlin@defra.gov.uk>
2024-03-28 12:18:13 +0000

    FSR-000 | fix missing comment issue

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const createServer = require('./server')
 const pino = require('./server/lib/logging/pino')
 
+// add a comment
 module.exports = createServer()
   .then(server => {
     server.listener.requestTimeout = 0
